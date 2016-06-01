882d03a1027367604d89a41c7bef2efa86b3e193
mtmitchell13 <mtmitchell85@gmail.com>
2016-05-31 22:14:24 -0400

    report to data frame complete

--- readMLS.R
+++ readMLS.R
@@ -10,17 +10,28 @@ readMLS <- function(id) {
                       regexpr("<div class=\"report-row-label.{0,12}\">.*</div>|<div class=\"report-row-label normalWrap.{0,10}\">.*</div>|<div class=\".{0,6}l report-row-label.{0,21}\">.*</div>", 
                               doc))
   
+  stage[grepl("Kitch:", stage) & grepl("normalWrap", stage)] <- gsub("Kitch:", "KitchDesc:", 
+                                                                     stage[grepl("Kitch:", stage) & grepl("normalWrap", stage)])
+  
+  labs <- regmatches(stage, regexpr("<label>.+</label>", stage))
+  labs <- gsub("<label>|</label>|&nbsp;", "", labs)
+
   stage <- gsub("<div class=\"[clear ]*[l ]*report-row-label *[ normalWrap| fieldHide]*\">|</*label>|</div>|&nbsp;", 
                 "", stage)
   
-  labs <- regmatches(stage, regexpr("<label>.+</label>", stage))
-  labs <- gsub("<label>|</label>", "", labs)
-
   vals <- paste(stage, collapse = "")
   vals <- strsplit(vals, paste(labs, collapse = "|"))
   vals <- unlist(vals)
   vals <- vals[-1]
   
   df <- data.frame(labs, vals)
-    
+  df <- df[df$labs != ":", ]
+  df$labs <- gsub(":", "", df$labs)
+  df$vals <- as.character(df$vals)
+
+  df <- spread(df, key = labs, value = vals)
+  df$reportId <- id
+  
+  return(df)
+      
 }       
